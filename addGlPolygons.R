33368dfb91af7fbbf7ce44ad57be6e587f3994c3
Phileas Condemine <phileas.condemine@gmail.com>
2021-10-06 14:33:28 +0200

    first expe with leafgl

---
+++ addGlPolygons.R
@@ -0,0 +1,46 @@
+library(mapview)
+library(leaflet)
+library(leafgl)
+library(sf)
+library(colourvalues)
+library(magrittr)
+library(data.table)
+gadm=readRDS("gadm36_FRA_5_sf.rds")
+load("poly_com_simplif.RData")
+
+gadm_nom=gadm$NAME_5
+nom2=poly_com[["2015"]]@data$NOM_COM.y
+sum(is.na(nom2))
+sum(!gadm_nom%in%nom2)
+data=poly_com[["2015"]]@data
+data=data.table(data)
+data$nb_NA=rowSums(is.na(data))
+setorder(data,nb_NA)
+data=data[,.SD[1],by="NOM_COM.y"]
+gadm=merge(gadm,data,by.x="NAME_5",by.y="NOM_COM.y")
+gadm=sf::st_cast(gadm,"POLYGON")
+gadm=gadm[!is.na(gadm$Z_MOYEN),]
+# 
+# ch_lu = st_read("gis_osm_landuse_a_free_1.shp")
+# ch_lu = ch_lu[, c(1, 3, 4)] # don't handle NAs so far
+# ch_lu=sf::st_cast(ch_lu,"POLYGON")
+options(viewer = NULL)
+
+# A cause de valeurs extrêmes, il vaut mieux considérer les quantiles.
+cols = colour_values_rgb(Hmisc::cut2(gadm$mg60_publie,g = 5)%>%as.numeric,palette = "blue2red",#colour_palettes()
+                         include_alpha = FALSE) / 255
+
+system.time({
+  m = leaflet() %>%
+    addTiles()%>%
+    # addProviderTiles(provider = providers$CartoDB.DarkMatter) %>%
+    addGlPolygons(data = gadm, 
+                  color = cols, 
+                  popup = "mg60_publie",
+                  group = "NAME_5") %>%
+    leafem::addMouseCoordinates() %>%
+    setView(lng = 2, lat = 47, zoom = 6) %>%
+    addLayersControl(overlayGroups = "NAME_5")
+})
+
+m
